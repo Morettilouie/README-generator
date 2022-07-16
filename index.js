149e0369b58dd2ca801be8afa02c7020770b8c79
Louie Moretti <morettilouie@gmail.com>
2022-07-16 03:19:16 -0500

    initial files and bit of start up code

---
+++ index.js
@@ -0,0 +1,28 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return `
+    ## Description
+    ## Table of Contents
+    ## Installation
+    ## Usage
+    ## Credits
+    ## License
+    ## Badges
+    ## Features
+    ## How to Contribute
+    ## Tests
+    `
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
